05cda752724ce3d189984216e4c56898b7636048
Lucas Garcia <nahuellucasgarcia@gmail.com>
2022-03-07 20:43:11 -0300

    Completé la lógica para el tercer problema

--- question-3/README.md
+++ question-3/README.md
@@ -16,4 +16,25 @@
 
 # Razonamiento
 
-_Escribir aquí los supuestos asumidos, reflexiones y explicaciones de la solución_
+## Problema con la implementación inicial
+En primer lugar, el código que llama a la función parentesisChecker:
+`const isValid = parenthesisChecker(args);`
+Tiene un error lógico. Si consideramos que la función **parenthesisChecker** recibe por definición un String como parámetro nos encontramos conque args no es un String, sino un arreglo.
+Por lo que hay que cambiar esta línea de código así:
+`const isValid = parenthesisChecker(args[0]);`
+
+## Diseño del algoritmo
+### Idea inicial
+Para controlar parentesis en un String necesitamos encontrar un parentesis cerrando a cada parentesis que se abre.
+Una solución simple sería utilizar un contador que aumente con cada parentesis abierto y disminuya con cada parentesis cerrado, de forma tal que si luego de recorrer toda la cadena de texto no tenemos el contador en 0, sabemos que la cadena de texto no es válida.
+
+El problema con esta implementación es que es complicado implementarla con varios tipos de paréntesis:
+ Si le agregamos al problema {}, [], etc.
+ se vuelve dificil evaluar cadenas como: **[((]))**
+ **¿Cómo recordamos sin complicar demasiado el algoritmo que el corchete se abrió antes que el paréntesis?**
+ 
+### Solucion usando un stack
+Una forma simple de encarar el problema teniendo en cuenta este problema es utilizando un stack. 
+Cada vez que leamos un parentesis abriendo lo agregamos al stack.
+Cada vez que leamos un parentesis cerrando combrobamos cual es el elemento en la cima del stack. Si es igual al opuesto correspondiente del parentesis leido, entonces quitamos el último elemento del stack. Si no se da esta igualdad, entonces tenemos un error y termina el programa.
+ 
--- question-3/script.js
+++ question-3/script.js
@@ -5,19 +5,83 @@
  */
 
 const args = process.argv.slice(-1);
-console.log(`Running question #3 with args ${args}`)
+console.log(`Running question #3 with args ${args}`);
 
 /**
+ * Check if a character is of type parenthesis.
+ * @param {String} char - Character to be evaluated.
+ * @returns {int} Returns 1 if char is of type opening parenthesis; returns -1 if char is of type close parenthesis; returns 0 otherwise.
+**/
+function isParenthesis(char) {
+    var res = 0;
+    const openParenthesis = [
+        '(',
+        '[',
+        '{'
+    ];
+    const closeParenthesis = [
+        ']',
+        ')',
+        '}',
+    ];
+    if (openParenthesis.includes(char))
+        res = 1;
+    else if (closeParenthesis.includes(char))
+        res = -1;
+
+    return res;
+}
+
+/**
+ * Given a char with a closing parenthesis, returns the corresponding opening parenthesis.
+ * @param {String} char - Character with a closing parenthesis.
+ * @returns {int} Returns the corresponding opening parenthesis of char. If char is not a valid character, reeturns an empty string.
+**/
+function getOpeningPraenthesis(char) {
+    var res = "";
+    switch(char) {
+    case ")":
+        res = "(";
+        break;
+    case "]":
+        res = "[";
+        break;
+    case "}":
+        res = "{";
+        break;
+    }
+    return res;
+}
+/**
  * Check if a string has correct use of parenthesis.
- * 
+ *
  * @param {String} str - String to be evaluated
  * @returns {Boolean} Returns true if string is valid.
  */
 function parenthesisChecker(str) {
-  // TODO: Implement validation logic
-  return true;
+    const stackParenthesis = [];
+    var error = false;
+
+    for (var i = 0; i < str.length && !error; i++) {
+        // console.log('----------------------------------------------');
+        var char = str[i];
+        const parenthesisType = isParenthesis(char);
+        if (parenthesisType > 0)
+            stackParenthesis.push(char);
+        else if (parenthesisType < 0) {
+            // console.log('stackParenthesis: ' + stackParenthesis[stackParenthesis.length - 1]);
+            // console.log('char:             ' + getOpeningPraenthesis(char));
+            // console.log('equals : ' + (stackParenthesis[stackParenthesis.length - 1].localeCompare(char) == 0));
+            if (stackParenthesis.length != 0 && stackParenthesis[stackParenthesis.length - 1].localeCompare(getOpeningPraenthesis(char)) == 0)
+                stackParenthesis.pop();
+            else
+                error = true;
+        }
+        // console.log('error: ' + error);
+    }
+    return !error && stackParenthesis.length == 0;
 }
 
-const isValid = parenthesisChecker(args);
-console.log(`parenthesisChecker("${args}") = ${isValid}`);
 
+const isValid = parenthesisChecker(args[0]);
+console.log(`parenthesisChecker("${args}") = ${isValid}`);
